174095484749201ac13b05da4b676181d0711379
weixian <e1398114@u.nus.edu>
2025-08-22 13:19:26 +0800

    Abstract formatting of print statements into function

--- src/main/java/Yappy.java
+++ src/main/java/Yappy.java
@@ -3,16 +3,20 @@ import java.util.Scanner;
 
 public class Yappy {
 
+    public static void printFormat(String[] messages) {
+        System.out.println("\t____________________________________________________________");
+        for (String message : messages) {
+            System.out.println(message);
+        }
+        System.out.println("\t____________________________________________________________");
+    }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws YappyException{
 
         Task[] library = new Task[100];
         int numOfItems = 0;
 
-
-        System.out.println("____________________________________________________________");
-        System.out.println("\tHello! I'm Yappy\n" + "\tWhat can I do for you?");
-        System.out.println("____________________________________________________________");
+        printFormat(new String[]{"\t\sHello! I'm Yappy\n" + "\t\sWhat can I do for you?"});
 
         Scanner input = new Scanner(System.in);
         while (true) {
@@ -20,31 +24,38 @@ public class Yappy {
             if (inputLine.equals("bye")) {
                 break;
             } else if (inputLine.equals("list")) {
-                System.out.println("\t____________________________________________________________");
-                System.out.println("\tHere are the tasks in your list:");
+
+                String[] messages = new String[numOfItems + 1];
+                messages[0] = "\t\sHere are the tasks in your list:";
                 for (int i = 0; i < numOfItems; i++) {
                     int num = i + 1;
                     Task task = library[i];
-                    System.out.println("\t" + num + "." + task.toString());
+                    messages[i + 1] = "\t\s" + num + "." + task.toString();
                 }
-                System.out.println("\t____________________________________________________________");
-                continue;
+                printFormat(messages);
             } else if (inputLine.startsWith("mark")) {
+                if (!inputLine.trim().contains(" ")) {
+                    throw new YappyException("Please specify the index of the task you wish to mark as completed!");
+                } else if (Integer.parseInt(inputLine.split(" ")[1]) - 1 > numOfItems) {
+                    throw new YappyException("Please specify a valid task index.\n\t\sNow you have " + numOfItems + (numOfItems==1 ? " task ": " tasks ") + "in the list.");
+                }
                 int index = Integer.parseInt(inputLine.split(" ")[1]) - 1;
                 library[index].setCompleted();
-                System.out.println("\t____________________________________________________________");
-                System.out.println("\tAlrighty I have marked this task as completed: " + "\n\t\s\s" + library[index].toString());
-                System.out.println("\t____________________________________________________________");
+                printFormat(new String[]{"\t\sAlrighty I have marked this task as completed:" + "\n\t\s\s" + library[index].toString()});
             } else if (inputLine.startsWith("unmark")) {
+                if (!inputLine.trim().contains(" ")) {
+                    throw new YappyException("Please specify the index of the task you wish to mark as uncompleted!");
+                }
                 int index = Integer.parseInt(inputLine.split(" ")[1]) - 1;
                 library[index].setUncompleted();
-                System.out.println("\t____________________________________________________________");
-                System.out.println("\tAlrighty I have marked this task as not completed yet: " + "\n\t\s\s" + library[index].toString());
-                System.out.println("\t____________________________________________________________");
+                printFormat(new String[]{"\t\sAlrighty I have marked this task as not completed yet:" + "\n\t\s\s" + library[index].toString()});
             } else {
                 if (inputLine.startsWith("todo")) {
+                    if (inputLine.length() == 1) {
+                        throw new YappyException("Please specify the ");
+                    }
                     String description = inputLine.substring("todo".length()).trim();
-                    library[numOfItems] = new ToDo(inputLine);
+                    library[numOfItems] = new ToDo(description);
                 } else if (inputLine.startsWith("event")) {
                     String description = inputLine.substring("event".length()).trim().split("/from")[0];
                     String when = inputLine.split("/from")[1];
@@ -57,16 +68,13 @@ public class Yappy {
 
                 numOfItems++;
 
-                System.out.println("\t____________________________________________________________");
-                System.out.println("\tAdded the following task: \n\t\s\s" + library[numOfItems - 1].toString() );
-                System.out.println("\tNow you have " + numOfItems + (numOfItems==1 ? " task ": " tasks ") + "in you list:");
-                System.out.println("\t____________________________________________________________");
+
+                printFormat(new String[]{"\t\sAdded the following task:\n\t\s\s\s" + library[numOfItems - 1].toString(),
+                                        "\t\sNow you have " + numOfItems + (numOfItems==1 ? " task ": " tasks ") + "in the list."});
             }
         }
 
-        System.out.println("\t____________________________________________________________");
-        System.out.println("\tBye. Hope to see you again soon!");
-        System.out.println("\t____________________________________________________________");
+        printFormat(new String[]{"\tBye. Hope to see you again soon!"});
 
     }
 }
