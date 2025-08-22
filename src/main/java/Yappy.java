17a8f0f25515f781a1c2a63bc55c7696da527bb5
weixian <e1398114@u.nus.edu>
2025-08-22 16:04:18 +0800

    Add support for deleting tasks from the list

--- src/main/java/Yappy.java
+++ src/main/java/Yappy.java
@@ -1,10 +1,12 @@
+import java.sql.Array;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 
 public class Yappy {
 
     static int numOfItems;
-    static Task[] library = new Task[100];
+    static ArrayList<Task> library = new ArrayList<>();
 
 
     public static void printFormat(String[] messages) {
@@ -22,7 +24,7 @@ public class Yappy {
             messages[0] = "\t\sHere are the tasks in your list:";
             for (int i = 0; i < numOfItems; i++) {
                 int num = i + 1;
-                Task task = library[i];
+                Task task = library.get(i);
                 messages[i + 1] = "\t\s" + num + "." + task.toString();
             }
             printFormat(messages);
@@ -33,45 +35,53 @@ public class Yappy {
                 throw new YappyException("Please specify a valid task index.\n\t\sNow you have " + numOfItems + (numOfItems==1 ? " task ": " tasks ") + "in the list.");
             }
             int index = Integer.parseInt(inputLine.split(" ")[1]) - 1;
-            library[index].setCompleted();
-            printFormat(new String[]{"\t\sAlrighty I have marked this task as completed:" + "\n\t\s\s" + library[index].toString()});
+            library.get(index).setCompleted();
+            printFormat(new String[]{"\t\sAlrighty I have marked this task as completed:" + "\n\t\s\s" + library.get(index).toString()});
 
         } else if (inputLine.startsWith("unmark")) {
             if (!inputLine.trim().contains(" ")) {
                 throw new YappyException("Please specify the index of the task you wish to mark as uncompleted!");
             }
             int index = Integer.parseInt(inputLine.split(" ")[1]) - 1;
-            library[index].setUncompleted();
-            printFormat(new String[]{"\t\sAlrighty I have marked this task as not completed yet:" + "\n\t\s\s" + library[index].toString()});
+            library.get(index).setUncompleted();
+            printFormat(new String[]{"\t\sAlrighty I have marked this task as not completed yet:" + "\n\t\s\s" + library.get(index).toString()});
 
+        } else if (inputLine.startsWith("delete")) {
+            if (!inputLine.trim().contains(" ")) {
+                throw new YappyException("Please specify the index of the task you wish to delete");
+            }
+            int index = Integer.parseInt(inputLine.split(" ")[1]) - 1;
+            library.remove(index);
+            numOfItems--;
+            printFormat(new String[]{"\t\sAlrighty I have removed the following task:" + "\n\t\s\s" + library.get(index).toString(), "\t\sNow you have " + + numOfItems + (numOfItems==1 ? " task ": " tasks ") + "in the list."});
         } else {
             if (inputLine.startsWith("todo")) {
                 if (!inputLine.trim().contains(" ")) {
                     throw new YappyException("Please specify the todo task!");
                 }
                 String description = inputLine.substring("todo".length()).trim();
-                library[numOfItems] = new ToDo(description);
+                library.add(numOfItems, new ToDo(description));
             } else if (inputLine.startsWith("event")) {
                 if (!inputLine.trim().contains(" ")) {
                     throw new YappyException("Please specify the event task and time specifications!");
                 }
                 String description = inputLine.substring("event".length()).trim().split("/from")[0];
                 String when = inputLine.split("/from")[1];
-                library[numOfItems] = new Event(description, when);
+                library.add(numOfItems, new Event(description, when));
             } else if (inputLine.startsWith("deadline")) {
                 if (!inputLine.trim().contains(" ")) {
                     throw new YappyException("Please specify the deadline task and deadline!");
                 }
                 String description = inputLine.substring("deadline".length()).trim().split("/by")[0].strip();
                 String by = inputLine.split("/by")[1].strip();
-                library[numOfItems] = new Deadline(description, by);
+                library.add(numOfItems, new Deadline(description, by));
             } else {
                 throw new YappyException("Sorry!! I do not know what that command is.");
             }
 
             numOfItems++;
 
-            printFormat(new String[]{"\t\sAdded the following task:\n\t\s\s\s" + library[numOfItems - 1].toString(),
+            printFormat(new String[]{"\t\sAdded the following task:\n\t\s\s\s" + library.get(numOfItems - 1).toString(),
                     "\t\sNow you have " + numOfItems + (numOfItems==1 ? " task ": " tasks ") + "in the list."});
         }
     }
