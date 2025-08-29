55e034a3c3fb3c03e314c472012f55bde12fe25c
weixian <e1398114@u.nus.edu>
2025-08-29 12:30:51 +0800

    Create TaskList class to store tasks

---
+++ src/main/java/TaskList.java
@@ -0,0 +1,28 @@
+public class TaskList {
+
+    Task[] tasks;
+
+    public TaskList() {
+        this.tasks = new Task[]{};
+    }
+
+    public TaskList(Task[] tasks) {
+        this.tasks = tasks;
+    }
+
+    public void add(Task task) {
+
+    }
+
+    public void delete(Task task) {
+
+    }
+
+    /*
+    public void mark(int index) {
+
+    }
+
+     */
+
+}
