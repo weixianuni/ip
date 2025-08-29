92fbd686de7e4d4a9edb2dad7bb3e54ec356fbad
weixian <e1398114@u.nus.edu>
2025-08-29 13:55:31 +0800

    Update methods and use ArrayList

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -2,27 +2,45 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 
 public class Storage {
 
     File file;
 
+    /**
+     *
+     * @param filePath
+     */
     public Storage(String filePath) {
         this.file = Paths.get(filePath).toFile();
     }
 
-    public Task[] loadTask() throws YappyException{
-        return new Task[]{};
+    /**
+     *
+     * @return
+     * @throws YappyException
+     */
+    public ArrayList<Task> loadTask() throws YappyException{
+        return new ArrayList<>();
     }
 
-    public void save(TaskList tasks) throws YappyException{
+    /**
+     *
+     * @param tasks
+     * @throws YappyException
+     */
+    public void save(ArrayList<Task> tasks) throws YappyException{
 
         try {
             FileWriter fw = new FileWriter(file);
             BufferedWriter bfw = new BufferedWriter(fw);
 
-
+            for (Task task : tasks) {
+                bfw.write(task.fileString());
+            }
             bfw.close();
         } catch (IOException e) {
             throw new YappyException(e.getMessage());
