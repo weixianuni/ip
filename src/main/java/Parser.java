ef27e25ea6b78011c9e10932833b5bf93b2aa8c0
weixian <e1398114@u.nus.edu>
2025-08-29 13:06:55 +0800

    Add Parser class to parse commands

---
+++ src/main/java/Parser.java
@@ -0,0 +1,61 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Parser {
+
+    private static DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+
+
+    public static Command parse(String command) throws YappyException {
+        if (command.equals("list")) {
+            return new ListCommand();
+        } else if (command.startsWith("mark")) {
+            if (!command.trim().contains(" ")) {
+                throw new YappyException("Please specify the index of the task!");
+            } else {
+                return new MarkCommand(Integer.parseInt(command.split(" ")[1]) - 1);
+            }
+        } else if (command.startsWith("unmark")) {
+            if (!command.trim().contains(" ")) {
+                throw new YappyException("Please specify the index of the task!");
+            } else {
+                return new UnmarkCommand(Integer.parseInt(command.split(" ")[1]) - 1);
+            }
+        } else if (command.startsWith("delete")) {
+            if (!command.trim().contains(" ")) {
+                throw new YappyException("Please specify the index of the task you wish to delete");
+            } else {
+                return new DeleteCommand(Integer.parseInt(command.split(" ")[1]) - 1);
+            }
+        } else {
+            if (command.startsWith("todo")) {
+                if (!command.trim().contains(" ")) {
+                    throw new YappyException("Please specify the todo task!");
+                } else {
+                    String description = command.substring("todo".length()).trim().split("/from")[0];
+                    return new AddCommand(new ToDo(description, false));
+                }
+            } else if (command.startsWith("event")) {
+                if (!command.trim().contains(" ")) {
+                    throw new YappyException("Please specify the event task and time specifications!");
+                } else {
+                    String description = command.substring("event".length()).trim().split("/from")[0];
+                    String fromAndTo = command.split("/from")[1];
+                    LocalDateTime from = LocalDateTime.parse(fromAndTo.split("/to")[0].strip(), FORMATTER);
+                    LocalDateTime to = LocalDateTime.parse(fromAndTo.split("/to")[1].strip(), FORMATTER);
+                    return new AddCommand(new Event(description, false, from, to));
+                }
+            } else if (command.startsWith("deadline")) {
+                if (!command.trim().contains(" ")) {
+                    throw new YappyException("Please specify the deadline task and deadline!");
+                } else {
+                    String description = command.substring("deadline".length()).trim().split("/by")[0].strip();
+                    LocalDateTime by = LocalDateTime.parse(command.split("/by")[1].strip(), FORMATTER);
+                    return new AddCommand(new Deadline(description, false, by));
+                }
+            } else {
+                throw new YappyException("Sorry!! I do not know what that command is.");
+            }
+        }
+    }
+}
