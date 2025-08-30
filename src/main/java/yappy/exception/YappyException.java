b1d2c94a140cafd306052dbfe4bbffc7f6b6f157
weixian <e1398114@u.nus.edu>
2025-08-30 20:00:27 +0800

    Add JavaDocs comments fofr YappyException class

--- src/main/java/yappy/exception/YappyException.java
+++ src/main/java/yappy/exception/YappyException.java
@@ -1,12 +1,18 @@
 package yappy.exception;
 
+/**
+ *
+ */
 public class YappyException extends Exception {
 
+    /**
+     * Constructs a new {@code YappyException} object with the corresponding error message.
+     * This constructor calls the Exception superclass and initialises
+     * the message field.
+     *
+     * @param message Error message to be printed out to user.
+     */
     public YappyException(String message) {
         super(message);
     }
-
-
-
-
 }
