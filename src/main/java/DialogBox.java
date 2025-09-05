6aaeee10141d911525056545d31b5d6aea692f2d
weixian <e1398114@u.nus.edu>
2025-09-05 17:50:13 +0800

    Update build.gradle file mainClass

---
+++ src/main/java/DialogBox.java
@@ -0,0 +1,66 @@
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.Node;
+import javafx.scene.media.AudioClip;
+import javafx.scene.shape.Circle;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
+
+import java.io.IOException;
+import java.util.Collections;
+
+public class DialogBox extends HBox {
+
+    @FXML
+    private Label dialog;
+    @FXML
+    private ImageView displayPicture;
+    @FXML
+    public void initialize() {
+        Circle clip = new Circle(25, 25, 25); // (centerX, centerY, radius)
+        displayPicture.setClip(clip);
+    }
+
+    public DialogBox(String text, Image img) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(MainWindow.class.getResource("/view/DialogBox.fxml"));
+            fxmlLoader.setController(this);
+            fxmlLoader.setRoot(this);
+            fxmlLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        dialog.setText(text);
+        displayPicture.setImage(img);
+
+    }
+
+    /**
+     * Flips the dialog box such that the ImageView is on the left and text on the right.
+     */
+    private void flip() {
+        ObservableList<Node> tmp = FXCollections.observableArrayList(this.getChildren());
+        Collections.reverse(tmp);
+        getChildren().setAll(tmp);
+        setAlignment(Pos.TOP_LEFT);
+        dialog.getStyleClass().add("reply-label");
+    }
+
+    public static DialogBox getUserDialog(String text, Image img) {
+        return new DialogBox(text, img);
+    }
+
+    public static DialogBox getDukeDialog(String text, Image img) {
+        var db = new DialogBox(text, img);
+        db.flip();
+        return db;
+    }
+}
