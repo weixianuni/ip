913a7a9d85b8a3811d0bda55c1097ac23315b572
weixian <e1398114@u.nus.edu>
2025-09-13 20:45:24 +0800

    Add streams to find command

--- src/main/java/yappy/command/FindCommand.java
+++ src/main/java/yappy/command/FindCommand.java
@@ -2,6 +2,7 @@ package yappy.command;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 import yappy.backend.Storage;
 import yappy.backend.TaskList;
@@ -39,17 +40,10 @@ public class FindCommand extends Command {
      */
     @Override
     public String execute(TaskList taskList, Ui ui, Storage storage) throws YappyException {
-
-        ArrayList<Task> filteredList = new ArrayList<>();
-
         ArrayList<Task> tasks = taskList.getTasks();
-        for (Task task : tasks) {
-            for (String query : queries) {
-                if (task.getDescription().contains(query)) {
-                    filteredList.add(task);
-                }
-            }
-        }
+        ArrayList<Task> filteredList = tasks.stream()
+                .filter(task -> queries.stream().anyMatch(query -> task.getDescription().contains(query)))
+                .collect(Collectors.toCollection(ArrayList::new));
 
         if (filteredList.isEmpty()) {
             return ("No matching task description!");
