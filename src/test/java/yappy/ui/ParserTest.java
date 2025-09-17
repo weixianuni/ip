d672bd97f4ca6a4d9856ce54898e701bde883f87
weixian <e1398114@u.nus.edu>
2025-09-17 19:56:21 +0800

    Fix assertion in ParserTest

--- src/test/java/yappy/ui/ParserTest.java
+++ src/test/java/yappy/ui/ParserTest.java
@@ -14,6 +14,22 @@ import yappy.exception.YappyException;
 
 public class ParserTest {
 
+    private static final String COMMANDS_HELP =
+            """
+            \t I don't understand. Available commands:
+            \t - bye
+            \t - list
+            \t - find <query1,query2,...>
+            \t - mark <index>
+            \t - unmark <index>
+            \t - delete <index>
+            \t - todo <description>
+            \t - deadline <description> /by <date>
+            \t - event <description> /from <date> /to <date>
+            \t - reschedule <index> /from <date> /to <date>
+            \t - postpone <index> /by <date>
+            \t Note: dates must use the format yyyy-MM-dd HH:mm""";
+
     @Test
     public void testParse_exitCommand_returnExitCommand() throws YappyException {
         assertInstanceOf(ExitCommand.class, Parser.parse("bye"));
@@ -27,6 +43,6 @@ public class ParserTest {
     @Test
     public void testParseUnknownCommand() {
         YappyException e = assertThrows(YappyException.class, () -> Parser.parse("foobar"));
-        assertEquals("\t Sorry!! I do not know what that command is.", e.getMessage());
+        assertEquals(COMMANDS_HELP, e.getMessage());
     }
 }
